72797902e238ddced4fe096324065e4baa4ea2cf
Raghupentala728 <praghu@aapthitech.com>
2025-06-27 12:39:12 +0530

    updated fetch-app-stats- service

--- backend/src/fetch-app-stats/fetch-app-stats.service.ts
+++ backend/src/fetch-app-stats/fetch-app-stats.service.ts
@@ -132,7 +132,7 @@ export class FetchAppStatsService {
                     }
                 });
 
-                console.log(`Stored AppStats: feature= ${featureKey}, app=${appName}, count = ${count}`)
+                // console.log(`Stored AppStats: feature= ${featureKey}, app=${appName}, count = ${count}`)
             }
         }
        } catch (err){
